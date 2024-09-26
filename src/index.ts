e49e457591badb5ceb8dc5e1ab9a95a7f074dd06
Rodrigo <rtrancozo@gmail.com>
2024-09-26 15:48:09 -0300

    criação da conexão e dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+import "dotenv/config"
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
+
+
+app.listen(3000, ()=>{
+    console.log("servidor inicializado na porta 3000");
+    
+})
+
 
